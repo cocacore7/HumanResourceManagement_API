610d96760f8a46444039123b5bf3838c5801730e
Oscar Rodriguez <cocacore7@gmail.com>
2025-10-15 22:32:29 -0600

    HRM_API_1 2025-10-15 Oscar Rodriguez ADD Se arreglo DTO para servicio de correos

--- HRM_API.Core/Dtos/Mail/JobInterviewDto.cs
+++ HRM_API.Core/Dtos/Mail/JobInterviewDto.cs
@@ -4,7 +4,7 @@ namespace HRM_API.Core.Dtos.Authorization
     {
         public string Code { get; set; } = string.Empty;
         public string FullName { get; set; } = string.Empty;
-        public string Age { get; set; } = int.Empty;
+        public int Age { get; set; } = 0;
         public string Gender { get; set; } = string.Empty;
         public string Phone { get; set; } = string.Empty;
         public string Email { get; set; } = string.Empty;
