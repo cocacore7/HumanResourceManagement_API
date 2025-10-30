d35c042fd9c0bbead64816cf8aa424015135e747
Oscar Rodriguez <cocacore7@gmail.com>
2025-10-30 01:05:08 -0600

    HRM_API_1 2025-10-30 Oscar Rodriguez ADD Se revirtio null

--- HRM_API.Core/Dtos/Form/UpdateTextAnswerDBResponseDto.cs
+++ HRM_API.Core/Dtos/Form/UpdateTextAnswerDBResponseDto.cs
@@ -3,7 +3,7 @@
     public class UpdateTextAnswerDBResponseDto
     {
         public string AnswerType { get; set; } = string.Empty;
-        public string? ValueText { get; set; } = string.Empty;
+        public string ValueText { get; set; } = string.Empty;
         public DateTime UpdatedAt { get; set; } = DateTime.Now;
         public int? ResponseId { get; set; }
         public int? QuestionId { get; set; }
