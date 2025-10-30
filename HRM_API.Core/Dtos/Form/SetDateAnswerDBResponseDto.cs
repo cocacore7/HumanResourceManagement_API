6faad5a517d14068f4caab25639e897c397d23b9
Oscar Rodriguez <cocacore7@gmail.com>
2025-10-29 22:58:59 -0600

    HRM_API_1 2025-10-29 Oscar Rodriguez ADD Se agrego parametro valueDate para recibir valores null

--- HRM_API.Core/Dtos/Form/SetDateAnswerDBResponseDto.cs
+++ HRM_API.Core/Dtos/Form/SetDateAnswerDBResponseDto.cs
@@ -5,7 +5,7 @@
         public int? ResponseId { get; set; }
         public int? QuestionId { get; set; }
         public string AnswerType { get; set; } = string.Empty;
-        public string ValueDate { get; set; } = string.Empty;
+        public string? ValueDate { get; set; } = string.Empty;
         public DateTime CreatedAt { get; set; } = DateTime.Now;
         public DateTime UpdatedAt { get; set; } = DateTime.Now;
     }
