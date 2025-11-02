6dcc16aa487bf8ace775fec7b3744b07f17a67d9
danny1227 <danielcoroy1200@gmail.com>
2025-11-02 11:03:36 -0600

    ajusts para validar servicio

--- HRM_API.Application/Services/FormService.cs
+++ HRM_API.Application/Services/FormService.cs
@@ -290,6 +290,7 @@ namespace HRM_API.Application.Services
                 request?.Origin.State == EnumHelper.GetEnumDescription(PreApplicationFailStatusEnum.ExpNotLoaded) ||
                 request?.Origin.State == EnumHelper.GetEnumDescription(PreApplicationFailStatusEnum.ExpLoaded) ||
                 request?.Origin.State == EnumHelper.GetEnumDescription(PreApplicationFailStatusEnum.Hiring) ||
+                request?.Origin.State == EnumHelper.GetEnumDescription(PreApplicationFailStatusEnum.Hired) ||
                 request?.Origin.State == EnumHelper.GetEnumDescription(PreApplicationFailStatusEnum.RejectDiscard))
             {
                 var StatusfailValid = (bool)await _preApplicationRepository.UpdateStatusFailAsync(preApplication.FirstOrDefault()?.Id, request?.Origin.State);
